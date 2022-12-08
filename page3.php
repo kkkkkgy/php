68e917739c996590bc2c55398f1ab2b6ea76abb1
juminkyu55555 <116254818+juminkyu55555@users.noreply.github.com>
2022-12-08 16:30:55 +0900

    Add files via upload

---
+++ page3.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>로그인</title>
+</head>
+<style>
+    <?php include "header.css" ?>
+    main{
+        padding-top: 150px;
+        font-size: 20px;
+        text-align: center;
+    }
+    input{
+        font-size: 15px;
+    }
+    #login_submit {
+        font-size: 20px;
+    }
+    #signup_submit{
+        font-size: 20px;
+    }
+    main{
+        padding-top: 0;
+    }
+    </style>
+</style>
+<body>
+<?php include "header.html"; ?>
+<main>
+<table border = "1" width = "250" height = "500" align="left" >
+            <caption>맛집랭킹</caption>
+            <thead>
+                <tr align="center" bgcolor=>
+                <th></th>
+                   
+                </tr>
+            </thead>
+
+            <tbody>
+                
+                    <th></th>
+                    
+                </tr>
+                
+                 
+            </tbody>
+        </table>
+        
+        <table border = "1" width = "250" height = "250" align="center" >
+            <caption>점메추</caption>
+            <thead>
+                
+                <th></th>
+                	
+                   
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr align="center" bgcolor="white">
+                    <th></th>
+                    
+                </tr>
+                
+                 
+            </tbody>
+        </table>
+        
+        
+         <table border = "1" width = "250" height = "250" align="center" >
+            <caption></caption>
+            <thead>
+                
+                <th></th>
+                	
+                   
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr align="center" bgcolor="white" align="center">
+                    <th></th>
+                    
+                </tr>
+                
+                 
+            </tbody>
+        </table>
+        
+        
+        <table border = "1" width = "200" height = "300" align="right" >
+            <caption>리뷰</caption>
+            <thead>
+                <tr align="center" bgcolor=>
+                <th></th>
+                   
+                </tr>
+            </thead>
+
+            <tbody>
+                
+                    <th></th>
+                    
+                </tr>
+                
+                 
+            </tbody>
+        </table>
+        
+        
+        
+        
+        
+        
+        
+        
+
+          
+        
+        
+ 
+ 
+
+
+</main>
+
+</body>
+</html>
